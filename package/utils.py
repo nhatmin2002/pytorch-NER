da4d7c4340258da1de0e9fc301d302e5000edf72
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-03-26 19:04:16 +0700

    Update utils.py

--- package/utils.py
+++ package/utils.py
@@ -3,24 +3,33 @@ import json
 
 
 def read_clue_json(path):
-    """Read json file in CLUE dataset
-    """
-    lines = []
-    with open(path, 'r', encoding='utf-8') as f:
-        for line in f:
-            line = json.loads(line.strip())
-            text = line['text']
-            label_entities = line.get('label', None)
-            label = ['O'] * len(text)
-            if label_entities is not None:
-                for key, value in label_entities.items():
-                    for sub_name, sub_index in value.items():
-                        for start_index, end_index in sub_index:
-                            assert text[start_index:end_index + 1] == sub_name
-                            label[start_index] = 'B-' + key
-                            label[start_index + 1:end_index + 1] = ['I-' + key] * (len(sub_name) - 1)
-            lines.append({"text": text, "label": label})
-    return lines
+    tokens, labels = [], []
+        tmp_tok, tmp_lab = [], []
+        label_set = []
+        lines = []
+    
+        with open(path, 'r',encoding='utf8') as reader:
+            for line in reader:
+                if "IMGID" in line: 
+                    a=1
+                else:
+                    line = line.strip()
+                    cols = line.split('\t')
+                    if len(cols) < 2:
+                        if len(tmp_tok) > 0:
+                            tokens.append(tmp_tok); labels.append(tmp_lab)
+                        tmp_tok = []
+                        tmp_lab = []
+                    else:
+                        tmp_tok.append(cols[0])
+                        tmp_lab.append(cols[-1])
+                        label_set.append(cols[-1])
+                        
+        dict_list = []
+        for token_seq, label_seq in zip(tokens, labels):
+            text = ' '.join(token_seq)  # Ghép các từ lại thành một chuỗi văn bản
+            dict_list.append({'text': text, 'label': label_seq})
+    return  dict_list
 
 
 def decode_bio_tags(tags):
