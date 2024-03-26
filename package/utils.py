6b8c753644563d4b1fff557dc70d9757d8cb76e3
nhatmin2002 <134791579+nhatmin2002@users.noreply.github.com>
2024-03-26 19:10:31 +0700

    Update utils.py

--- package/utils.py
+++ package/utils.py
@@ -4,11 +4,11 @@ import json
 
 def read_clue_json(path):
     tokens, labels = [], []
-        tmp_tok, tmp_lab = [], []
-        label_set = []
-        lines = []
+    tmp_tok, tmp_lab = [], []
+    label_set = []
+    lines = []
     
-        with open(path, 'r',encoding='utf8') as reader:
+    with open(path, 'r',encoding='utf8') as reader:
             for line in reader:
                 if "IMGID" in line: 
                     a=1
@@ -25,8 +25,8 @@ def read_clue_json(path):
                         tmp_lab.append(cols[-1])
                         label_set.append(cols[-1])
                         
-        dict_list = []
-        for token_seq, label_seq in zip(tokens, labels):
+    dict_list = []
+    for token_seq, label_seq in zip(tokens, labels):
             text = ' '.join(token_seq)  # Ghép các từ lại thành một chuỗi văn bản
             dict_list.append({'text': text, 'label': label_seq})
     return  dict_list
